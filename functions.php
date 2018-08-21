8b89bf15963c39b2be65f3cc123c4fb3982562e3
todd-uams <todd@uams.edu>
2018-08-21 15:40:51 -0500

    Initial Commit & Build

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Add an automatic default custom taxonomy for calendar.
+* If no event (taxonomy) is set, the event will be sorted as “draft” and won’t return an offset error.
+*
+*/
+
+// Fix ACF Support
+add_filter('_includes/acf-pro/settings/show_admin', '__return_true');
+
+/* Allow custom Quicklinks */
+require( 'setup/class.uams-quicklinks.php' );
